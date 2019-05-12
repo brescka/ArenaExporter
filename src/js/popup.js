666f82c07cfc1d10a6f7e1b6aec33a69932a5c40
Benjamen Brescka <benjamen.brescka@gmail.com>
2019-05-12 16:09:08 -0500

    Final UI changes to icon popup.

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const select = document.querySelectorAll('select')[0]
-  M.FormSelect.init(select)
   chrome.storage.local.get('language', (data) => {
     const language = Object.keys(data).includes('language') ? data.language : 'English'
     select.value = language
+    M.FormSelect.init(select)
     select.addEventListener('change', function () {
       const language = this.value
       chrome.storage.local.set({ language })
