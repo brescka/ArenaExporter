e0ac93708953196498211bc1158555302964ec6c
Benjamen Brescka <benjamen.brescka@gmail.com>
2019-05-12 14:32:47 -0500

    Modified transform function to account for errors in data.

--- transform.js
+++ transform.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const standardJSON = JSON.parse(fs.readFileSync('data/standard.json'))
 const reducedSets = []
+// For some reason MTGJSON does not have translations for some of the basic
+// land printings, so here we catalogue them for later double checks.
+const basicLands = ['Island', 'Plains', 'Swamp', 'Mountain', 'Forest']
 for (setName of Object.keys(standardJSON)) {
   const setCards = standardJSON[setName].cards
   const setTranslations = standardJSON[setName].translations
@@ -17,6 +20,13 @@ for (setName of Object.keys(standardJSON)) {
       name: card.name,
       number: card.number
     }
+  }).filter((card)=>{
+    // Filtering mythic edition cards
+    return !card.number.includes('★')
+  }).filter((card)=>{
+    // WAR cards are guarenteed to have translation data. Will open pull request
+    // with MTGJSON to address the basic lands that do not contain translation data.
+    return !(basicLands.includes(card.name) && setName !== 'WAR')
   })
   const name = setName === "DOM" ? "DAR" : setName
   const reducedSet = {
