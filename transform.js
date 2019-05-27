3048c81942f2c6b815e06daefa463671bd86a5d0
Benjamen Brescka <benjamen.brescka@gmail.com>
2019-05-27 16:08:16 -0500

    Removed extraneous code from transform.

--- transform.js
+++ transform.js
@@ -1,9 +1,6 @@
 const fs = require('fs')
 const standardJSON = JSON.parse(fs.readFileSync('data/standard.json'))
 const reducedSets = []
-// For some reason MTGJSON does not have translations for some of the basic
-// land printings, so here we catalogue them for later double checks.
-const basicLands = ['Island', 'Plains', 'Swamp', 'Mountain', 'Forest']
 for (const setName of Object.keys(standardJSON)) {
   const setCards = standardJSON[setName].cards
   const setTranslations = standardJSON[setName].translations
